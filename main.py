319dbcf723d6725295ad0f747d2dc63413e06fde
Lucas Sousa <sousa.lucas5376@gmail.com>
2024-02-22 21:14:12 -0300

    aaaaa

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def user():
 @app.route("/contextorequisicao")
 def context():
     user_agent = request.headers.get('User-Agent')
-    url = request.host_url
-    ip = request.remote_addr
+    url = request.remote_addr
+    ip = request.host_url
     return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do computador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><p><a href="/">Voltar</a></p>'.format(user_agent, url, ip)
 
 if __name__ == '__main__':
